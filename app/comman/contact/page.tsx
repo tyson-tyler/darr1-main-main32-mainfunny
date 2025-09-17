4a940e39f17b4874b90bcf3de110d6c0288396ba
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2025-09-17 12:51:40 +0530

    Update page.tsx

--- app/comman/contact/page.tsx
+++ app/comman/contact/page.tsx
@@ -54,7 +54,7 @@ export default function ContactForm() {
 Email: ${formData.user_email} 
 Phone: ${formData.phone} 
 Message: ${formData.message}`;
-  const whatsappLink = `https://wa.me/9990236468?text=${encodeURIComponent(
+  const whatsappLink = `https://wa.me/919990236468?text=${encodeURIComponent(
     whatsappMessage
   )}`;
 
